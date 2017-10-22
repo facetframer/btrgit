ce40ec69d16e48495e27bd9e3be72ab01b5ea15e
Facet <facetframer@googlemail.com>
2017-10-22 18:55:38 +0200

    Add log subcommand in preparation for restore and cat commands.
    Make log accept paths rather than mountpounts.

--- btrlog/btrlog.py
+++ btrlog/btrlog.py
@@ -7,30 +7,36 @@ import collections
 import itertools
 import logging
 import os
+import os.path
 import pipes
 import re
 import subprocess
+import sys
+
 import dateutil.parser
 
 LOGGER = logging.getLogger()
 
 def build_parser():
-    parser = argparse.ArgumentParser(prog='btrlog', description='If you have btrfs snapshots show how they change over time')
+    parser = argparse.ArgumentParser(prog='btrlog', description='An interface to btrfs that behaves like git')
     parser.add_argument('--debug', action='store_true', help='Print debug output')
-    parser.add_argument('mount', type=str, help='While system to operate on')
-    parser.add_argument('commit', nargs='?', help='Show what changed in this snapshot (regular expression)', type=str)
-    parser.add_argument('--no-files', action='store_true', help='Do not display the files that have changed')
+    parsers = parser.add_subparsers(dest='command')
 
-    show_mx = parser.add_mutually_exclusive_group()
-    show_mx.add_argument('--all', nargs='?', help='Show all changes before')
-    show_mx.add_argument('--single', nargs='?', help='Show just this change')
+    log_parser = parsers.add_parser('log', help='Show changes made to a file')
+    log_parser.add_argument('path', type=str, help='Path to operate on')
+    log_parser.add_argument('commit', nargs='?', help='Show what changed in this snapshot (regular expression)', type=str)
+    log_parser.add_argument('--no-files', action='store_true', help='Do not show files that have changed (just snapshots)')
+
+    show_mx = log_parser.add_mutually_exclusive_group()
+    show_mx.add_argument('--all', action='store_true', help='Show all changes before')
+    show_mx.add_argument('--single', action='store_true', help='Show just this change')
     return parser
 
 Snapshot = collections.namedtuple('Snapshot', 'snapshot transaction old_transaction')
 
 def get_subvolumes(mount):
     string = subprocess.check_output(
-        "/usr/bin/sudo btrfs subvolume list {} | cut -d ' ' -f 4,9".format(pipes.quote(mount)),
+        "btrfs subvolume list {} | cut -d ' ' -f 4,9".format(pipes.quote(mount)),
         shell=True)
     transactions = []
     snapshots = []
@@ -51,14 +57,40 @@ def get_subvolumes(mount):
     snapshots = itertools.starmap(Snapshot, info)
     return snapshots
 
+def mount_points():
+    with open('/proc/mounts') as stream:
+         return [x for x in [x.split()[1] for x in stream.read().splitlines()] if x.startswith('/')]
+
+def find_mount(mount_points, path):
+    path = os.path.abspath(path)
+    best_mount = ''
+    path = path.rstrip('/')
+    for mount in mount_points:
+        mount = mount.rstrip('/')
+        mount_list = mount.split('/')
+        if os.path.commonprefix([mount, path]) == mount:
+            if len(mount) > len(best_mount):
+                best_mount = mount
+
+
+    if best_mount == '':
+        raise Exception('Could not find mount')
+    else:
+        return best_mount
 
 def main():
+    LOGGER.debug('Starting')
     args = build_parser().parse_args()
 
     if args.debug:
         logging.basicConfig(level=logging.DEBUG)
 
-    subvolumes = get_subvolumes(args.mount)
+    path_list = args.path.split('/')
+
+    mount = find_mount(mount_points(), args.path)
+    LOGGER.debug('%r is under mount: %r', args.path, mount)
+
+    subvolumes = get_subvolumes(mount)
 
     if args.commit:
         subvolumes = [x for x in subvolumes if re.search(args.commit, x.snapshot)]
@@ -68,17 +100,23 @@ def main():
         elif len(subvolumes) != 1:
             raise Exception('Matches multiple commits')
 
-
     for subvolume in subvolumes:
         creation_time = get_creation_time(subvolume.snapshot)
         LOGGER.debug('Getting changed files for %r', subvolume)
-        if args.no_files:
-            print creation_time, subvolume.snapshot
-        else:
-            changed_files = find_changed_files(args.mount, subvolume)
-            for filename in changed_files:
-                print creation_time, subvolume.snapshot, filename
+        changed_files = find_changed_files(mount, subvolume)
+        for filename in changed_files:
+            if args.no_files:
+                print creation_time, subvolume.snapshot
+                break
+            else:
+                if is_subpath(args.path, filename):
+                    print creation_time, subvolume.snapshot, filename
+                    sys.stdout.flush()
 
+def is_subpath(prefix, path):
+    prefix = prefix.rstrip('/')
+    path = path.rstrip('/')
+    return os.path.commonprefix([prefix, path]) == prefix
 
 def get_creation_time(snapshot):
     data = subprocess.check_output(['btrfs', 'subvolume', 'show', snapshot])
